0a0b89049e73be0901c11c5285213e3ad59b3d81
aaronarr <arobinson@student.mtsac.edu>
2015-06-07 14:12:15 -0700

    Hard coded services

--- html/services.php
+++ html/services.php
@@ -8,7 +8,30 @@ include ('fns.php');
 do_html_header('Services');
 ?>
 
+<div>
+    <div><h3>Security</h3>
+    Apache Security - Apache web server security system deployment
+    Nginx Security - Nginx web server security system deployment
+    </div>
 
+    <div>
+    <h3>Home Improvement</h3>
+    Plumbing - Piping, water systems, sewage systems
+    Landscaping - Front and backyard terrain shaping
+    </div>
+
+    <div>
+    <h3>Automobile</h3>
+    Auto A/C - Automobile air conditioning
+    Auto Sound Systems - Automobile sound systems install and repair
+    </div>
+
+    <div>
+    <h3>Website Development</h3>
+    PHP development - PHP code for web servers
+    HTML development - HTML code for web servers
+    </div>
+</div>
 
 <?php
 do_html_footer();
